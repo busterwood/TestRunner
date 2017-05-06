805804007128020707e66ec37a41b7f5b1a9bfa6
busterwood <chris.j.austin@gmail.com>
2017-05-06 14:44:50 +0100

    Refactor FixtureRunner extracting out the TestRunner class

--- Shared.Test/FixtureRunner.cs
+++ Shared.Test/FixtureRunner.cs
@@ -2,8 +2,6 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Test
 {
@@ -22,7 +20,6 @@ namespace Test
         int failed;
         int ignored;
         Stopwatch watch;
-        int testFinished;
 
         public Stats Statistics => new Stats(testCount, passed, failed, ignored);
 
@@ -59,31 +56,37 @@ namespace Test
         {
             obj = Activator.CreateInstance(fixture);
             FixtureSetUp();
-            foreach (var test in methods)
+            foreach (var testMethod in methods)
             {
-                watch.Reset();
-                watch.Start();
-                if (test.IsIgnored())
+                if (testMethod.IsIgnored())
                 {
-                    StdOut.Ignore($"{fixture.Name}.{test.Name}");
+                    StdOut.Ignore($"{fixture.Name}.{testMethod.Name}");
                     ignored++;
                     testCount++;
                 }
-                else if (test.IsTest())
+                else if (testMethod.IsTest())
                 {
                     testCount++;
-                    RunTestLifeCycle(test, null, test.Name);
-                    continue;
+                    var test = CreateTest(testMethod);
+                    RunTest(test);
                 }
-                else foreach (var testCase in test.CustomAttributes.Where(a => a.IsTestCase()))
+                else foreach (var testCase in testMethod.CustomAttributes.Where(a => a.IsTestCase()))
                 {
                     testCount++;
-                    var args = TestCaseArgs(testCase);
-                    var testName = TestCaseName(test, args);
-                    RunTestLifeCycle(test, args, testName);
+                    var test = CreateTestCase(testMethod, testCase);
+                    RunTest(test);
                 }
             }
             FixtureTearDown();
+        }
+
+        private void RunTest(TestRunner test)
+        {
+            test.Run();
+            if (test.Passed)
+                passed++;
+            else if (test.Failed)
+                failed++;
         }
 
         private bool FixtureSetUp()
@@ -106,7 +109,29 @@ namespace Test
             }
         }
 
-        private bool FixtureTearDown()
+        TestRunner CreateTest(MethodInfo testMethod)
+        {
+            return new TestRunner(fixture.Name, testMethod.Name, obj, setup, tearDown, testMethod, fixtureTimeoutMs, watch, null);
+        }
+
+        TestRunner CreateTestCase(MethodInfo testMethod, CustomAttributeData testCase)
+        {
+            var args = TestCaseArgs(testCase);
+            var testName = TestCaseName(testMethod, args);
+            return new TestRunner(fixture.Name, testName, obj, setup, tearDown, testMethod, fixtureTimeoutMs, watch, args);
+        }
+
+        static object[] TestCaseArgs(CustomAttributeData testCase)
+        {
+            return testCase.ConstructorArguments.Select(arg => arg.Value).ToArray();
+        }
+
+        static string TestCaseName(MethodInfo test, object[] args)
+        {
+            return test.Name + "(" + string.Join(",", args.Select(a => a is string ? '"' + a.ToString() + '"' : a)) + ")";
+        }
+
+        bool FixtureTearDown()
         {
             try
             {
@@ -125,159 +150,5 @@ namespace Test
                 return false;
             }
         }
-
-        private static object[] TestCaseArgs(CustomAttributeData testCase)
-        {
-            return testCase.ConstructorArguments.Select(arg => arg.Value).ToArray();
-        }
-
-        private static string TestCaseName(MethodInfo test, object[] args)
-        {
-            return test.Name + "(" + string.Join(",", args.Select(a => a is string ? '"' + a.ToString() + '"' : a)) + ")";
-        }
-
-        private void RunTestLifeCycle(MethodInfo test, object[] args, string testName)
-        {
-            if (!SetUp(test))
-                return;
-            object timeout = TestTimeout(test) ?? fixtureTimeoutMs;
-            if (timeout != null)
-                RunTestWithTimeout(test, args, testName, timeout);
-            else
-                RunTest(test, args, testName);
-
-            TearDown(test);
-        }
-
-        private object TestTimeout(MethodInfo test)
-        {
-            return test.CustomAttributes
-                .FirstOrDefault(a => a.IsTimeout())
-                ?.ConstructorArguments
-                ?.First().Value;
-        }
-
-        private void RunTestWithTimeout(MethodInfo test, object[] args, string testName, object timeoutMs)
-        {
-            testFinished = 0;
-            Thread testThread = new Thread(() => RunTest(test, args, testName));
-            testThread.Start();
-
-            if (testThread.Join((int)timeoutMs))
-                return; // all good
-
-            testThread.Interrupt();
-            if (!testThread.Join(50))
-            {
-                // last chance - aborting may leave unrealased locks 
-                testThread.Abort();
-                testThread.Join();
-            }
-            Fail(testName, $"Timed-out after {(int)timeoutMs:N0} MS");
-        }
-
-        private void RunTest(MethodInfo test, object[] args, string testName)
-        {
-            Interlocked.Exchange(ref testFinished, 0);
-            try
-            {
-                var result = test.Invoke(obj, args) as Task; // support for asyn test methods
-                if (result != null)
-                    WaitForAsyncTestToComplete(testName, result);
-                else
-                    Pass(testName);
-            }
-            catch (TargetInvocationException ex)
-            {
-                Fail(testName, ex.InnerException);
-            }
-            catch (Exception ex)
-            {
-                Fail(testName, ex);
-            }
-        }
-
-        private void WaitForAsyncTestToComplete(string testName, Task result)
-        {
-            try
-            {
-                result.Wait(); 
-                Pass(testName);
-            }
-            catch (AggregateException ex) when (ex.InnerException is TargetInvocationException)
-            {
-                Fail(testName, ex.InnerException.InnerException);
-            }
-            catch (AggregateException ex)
-            {
-                Fail(testName, ex.InnerException);
-            }
-        }
-
-        private void Pass(string testName)
-        {
-            if (EndOfTestAlreadyReported()) return;
-            StdOut.Passed($"{fixture.Name}.{testName} in {watch.ElapsedMilliseconds:D0} MS");
-            passed++;
-        }
-
-        private void Fail(string testName, Exception ex)
-        {
-            if (EndOfTestAlreadyReported()) return;
-            Console.WriteLine(ex);
-            StdOut.Fail($"{fixture.Name}.{testName} in {watch.ElapsedMilliseconds:D0} MS");
-            failed++;
-        }
-
-        private void Fail(string testName, string message)
-        {
-            if (EndOfTestAlreadyReported()) return;
-            Console.WriteLine(message);
-            StdOut.Fail($"{fixture.Name}.{testName} in {watch.ElapsedMilliseconds:D0} MS");
-            failed++;
-        }
-
-        private bool EndOfTestAlreadyReported() => Interlocked.CompareExchange(ref testFinished, 1, 0) == 1;
-
-        private bool SetUp(MethodInfo test)
-        {
-            try
-            {
-                if (setup != null)
-                    setup.Invoke(obj, null);
-                return true;
-            }
-            catch (TargetInvocationException ex)
-            {
-                StdErr.Error($"{fixture.Name}: Failed to setup {test.Name}: {ex.InnerException}");
-                return false;
-            }
-            catch (Exception ex)
-            {
-                StdErr.Error($"{fixture.Name}: Failed to setup {test.Name}: {ex}");
-                return false;
-            }
-        }
-
-        private bool TearDown(MethodInfo test)
-        {
-            try
-            {
-                if (tearDown != null)
-                    tearDown.Invoke(obj, null);
-                return true;
-            }
-            catch (TargetInvocationException ex)
-            {
-                StdErr.Error($"{fixture.Name}: Failed to tear down {test.Name}: {ex.InnerException}");
-                return false;
-            }
-            catch (Exception ex)
-            {
-                StdErr.Error($"{fixture.Name}: Failed to tear down {test.Name}: {ex}");
-                return false;
-            }
-        }
-
     }
 }
---
+++ Shared.Test/TestRunner.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class TestRunner
+    {
+        readonly string fixtureName;
+        readonly string testName;
+        readonly object obj;
+        readonly MethodInfo setup;
+        readonly MethodInfo tearDown;
+        readonly MethodInfo test;
+        readonly object fixtureTimeoutMs;
+        readonly Stopwatch watch;
+        readonly object[] args;
+        int testFinished;
+
+        public TestRunner(string fixtureName, string testName, object obj, MethodInfo setup, MethodInfo tearDown, MethodInfo test, object fixtureTimeoutMs, Stopwatch watch, object[] args)
+        {
+            this.fixtureName = fixtureName;
+            this.testName = testName;
+            this.obj = obj;
+            this.setup = setup;
+            this.tearDown = tearDown;
+            this.test = test;
+            this.fixtureTimeoutMs = fixtureTimeoutMs;
+            this.watch = watch;
+            this.args = args;
+        }
+
+        public bool Ignored => test.IsIgnored();
+
+        public bool Passed { get; private set; }
+
+        public bool Failed { get; private set; }
+
+        public void Run()
+        {
+            watch.Reset();
+            watch.Start();
+            if (!SetUp())
+                return;
+            object timeout = TestTimeout() ?? fixtureTimeoutMs;
+            if (timeout != null)
+                RunTestWithTimeout(args, timeout);
+            else
+                RunTestMethod();
+            TearDown();
+        }
+
+        private object TestTimeout()
+        {
+            return test.CustomAttributes.FirstOrDefault(a => a.IsTimeout())?.ConstructorArguments?.First().Value;
+        }
+
+        private void RunTestWithTimeout(object[] args, object timeoutMs)
+        {
+            Thread testThread = new Thread(RunTestMethod)
+            {IsBackground = true};
+            testThread.Start();
+            if (testThread.Join((int)timeoutMs))
+                return; // all good
+            testThread.Interrupt();
+            if (!testThread.Join(50))
+            {
+                // last chance - aborting may leave unrealased locks 
+                testThread.Abort();
+                testThread.Join();
+            }
+            Fail($"Timed-out after {(int)timeoutMs} MS");
+        }
+
+        private void RunTestMethod()
+        {
+            try
+            {
+                var result = test.Invoke(obj, args) as Task; // support async test methods
+                if (result != null)
+                    WaitForAsyncTestToComplete(result);
+                else
+                    Pass();
+            }
+            catch (TargetInvocationException ex)
+            {
+                Fail(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                Fail(ex);
+            }
+        }
+
+        private void WaitForAsyncTestToComplete(Task result)
+        {
+            try
+            {
+                result.Wait();
+                Pass();
+            }
+            catch (AggregateException ex)when (ex.InnerException is TargetInvocationException)
+            {
+                Fail(ex.InnerException.InnerException);
+            }
+            catch (AggregateException ex)
+            {
+                Fail(ex.InnerException);
+            }
+        }
+
+        private void Pass()
+        {
+            if (EndOfTestAlreadyReported())
+                return;
+            StdOut.Passed($"{fixtureName}.{testName} in {watch.ElapsedMilliseconds} MS");
+            Passed = true;
+        }
+
+        private void Fail(Exception ex)
+        {
+            if (EndOfTestAlreadyReported())
+                return;
+            Console.WriteLine(ex); // write out the message before the fail line so the Gui can parse the result
+            StdOut.Fail($"{fixtureName}.{testName} in {watch.ElapsedMilliseconds} MS");
+            Failed = true;
+        }
+
+        private void Fail(string message)
+        {
+            if (EndOfTestAlreadyReported())
+                return;
+            Console.WriteLine(message); // write out the message before the fail line so the Gui can parse the result
+            StdOut.Fail($"{fixtureName}.{testName} in {watch.ElapsedMilliseconds} MS");
+            Failed = true;
+        }
+
+        private bool EndOfTestAlreadyReported() => Interlocked.CompareExchange(ref testFinished, 1, 0) == 1;
+
+        private bool SetUp()
+        {
+            try
+            {
+                setup?.Invoke(obj, null);
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                Fail($"SetUp failed: {ex.InnerException}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Fail($"SetUp failed: {ex}");
+                return false;
+            }
+        }
+
+        private bool TearDown()
+        {
+            try
+            {
+                tearDown?.Invoke(obj, null);
+                return true;
+            }
+            catch (TargetInvocationException ex)
+            {
+                Fail($"TearDown failed: {ex.InnerException}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Fail($"TearDown failed: {ex}");
+                return false;
+            }
+        }
+    }
+}
